63cfba97e65b774633d65accf39d86ed834eb77d
Shazia456 <128918032+Shazia456@users.noreply.github.com>
2023-05-16 08:29:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+// //  Ques#1
+// let name = ("shazia"); let age = 21; let favColor = ("pink");
+
+
+// // Ques#2 legal variables
+// let myName = "shazia";
+// let myAge = "21 years";
+// let favColor = "pink";
+// let weight = "56kg";
+// let $income = 15000;
+
+// // illegal variables
+
+// let my Name ="shazia";
+// let house# = 124;
+// let 3favColors ="pink,blue,green";
+// let weight + height = 56;
+// let alert = "hello world";
+
+
+// // Ques#3
+document.write("<b> Rules for naming JS variables");
+document.write( "<br> <b> Variables name can only contain letters,numbers,$ and _. For example $my_1st variable");
+document.write("<br> variable must begin with a $sign , underscore and letter. For example $name,_name or name");
+document.write("<br> Variable names are case sensitive");
+document.write("<br> Variable names should not be JS keywords");
+
+
+
+
